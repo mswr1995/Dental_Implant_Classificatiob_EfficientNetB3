7fb167c6cd6619ea8319a962ae0e1655626c1492
Musawer Muradi <61885420+mswr1995@users.noreply.github.com>
2025-04-05 17:42:57 +0300

    Delete run_debug.sh
    unnecessary file deletedd

--- run_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Starting dental implant debug script..."
-PYTHONPATH=/home/mswr/Automated\ Dental\ Implant\ Recognition\ in\ Radiographic\ Images\ Transfer\ Learning\ Implementation\ with\ EfficientNetB3
-python3 debug/dental_implant_debug.py "$@" 2>&1 | tee debug_output.log
-echo "Script execution completed. Output logged to debug_output.log"
